7e001cb89d473ad0d153501beeb2547dbd5c4da0
Ed Jones <ed.jones@canonical.com>
2023-09-27 15:58:42 +0100

    fix: Run formatter, add `Session` stub

--- src/instance.rs
+++ src/instance.rs
@@ -16,7 +16,7 @@
 
 // I have no idea if half of these should be strings but oh well
 pub struct Instance {
-    name: String,  // TODO: Ensure that this is read-only
+    name: String, // TODO: Ensure that this is read-only
     description: String,
     architecture: String,
     created_at: String,
@@ -26,13 +26,13 @@ pub struct Instance {
     expanded_config: String,
     expanded_devices: String,
     profiles: String,
-    status: String,  // TODO: Ensure that this is read-only
+    status: String,        // TODO: Ensure that this is read-only
     last_used_at: String,  // TODO: Ensure that this is read-only
-    location: String,  // TODO: Ensure that this is read-only
-    instance_type: String,  // TODO: Ensure that this is read-only
-    project: String, // TODO: Ensure this is read-only and optional.
-    status_code: String,  // TODO: Ensure that this is read-only
-    stateful: String, // TODO: Ensure that this is read-only
+    location: String,      // TODO: Ensure that this is read-only
+    instance_type: String, // TODO: Ensure that this is read-only
+    project: String,       // TODO: Ensure this is read-only and optional.
+    status_code: String,   // TODO: Ensure that this is read-only
+    stateful: String,      // TODO: Ensure that this is read-only
     snapshots: String,
     files: String,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -12,5 +12,5 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-mod instance;
 mod client;
+mod instance;
